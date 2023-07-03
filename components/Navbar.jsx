14fab3b1018cf5b677fbb0ce4e4387f32ddce136
Carlos Brazil <cabrazil@gmail.com>
2023-07-02 22:29:08 -0300

    finished portfolio

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
@@ -8,6 +10,27 @@ import { BsFillPersonLinesFill } from 'react-icons/bs'
 const Navbar = () => {
   const [nav, setNav] = useState(false)
   const [shadow, setShadow] = useState(false)
+  const [navBg, setNavBg] = useState('#ecf0f3');
+  const [linkColor, setLinkColor] = useState('#1f2937');
+  // const [position, setPosition] = useState('fixed')
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      router.asPath === '/property' ||
+      router.asPath === '/crypto' ||
+      router.asPath === '/netflix' ||
+      router.asPath === '/twitch'
+    ) {
+      setNavBg('transparent');
+      setLinkColor('#ecf0f3');
+    } else {
+      setNavBg('#ecf0f3');
+      setLinkColor('#1f2937');
+    }
+  }, [router]);
+
+
 
   const handleNav = () => {
     setNav(!nav)
@@ -25,7 +48,14 @@ const Navbar = () => {
   },[])
 
   return (
-    <div className={shadow ? 'fixed w-full h-25 shadow-xl z-[100]' : 'fixed w-full h-25 z-[100]' }>
+    <div 
+      style={{ backgroundColor: `${navBg}` }}
+      className={
+        shadow 
+        ? 'fixed w-full h-25 shadow-xl z-[100]' 
+        : 'fixed w-full h-25 z-[100]' 
+      }
+    >
       <div className = 'flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         <Link href='/'>
           <Image 
@@ -36,9 +66,11 @@ const Navbar = () => {
           />
         </Link>
         <div>
-          <ul className='hidden md:flex'>
+          <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
             <Link href='/'>
-              <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+              <li className='ml-10 text-sm uppercase hover:border-b'>
+                Home
+              </li>
             </Link>
             <Link href='/#about'>
               <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
@@ -68,31 +100,43 @@ const Navbar = () => {
       >
       <div>
         <div className='flex w-full items-center justify-between'>
-          <Image src='/../public/assets/navLogo.png' width='87' height='35' alt='/' />
-          <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+          <Link href='/'>
+            <Image 
+              src='/../public/assets/navLogo.png' 
+              width='87' 
+              height='35' 
+              alt='/' 
+            />
+          </Link>
+          <div 
+            onClick={handleNav} 
+            className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'
+          >
             <AiOutlineClose />
           </div>
         </div>
         <div className='border-b border-gray-300 my-4'>
-          <p className='w-[85%] md:w-[90%] py-4'>Let's build something legendary together.</p>
+          <p className='w-[85%] md:w-[90%] py-4'>
+            Let's build something legendary together.
+          </p>
         </div>
       </div>
       <div className='py-4 flex flex-col'>
         <ul className='uppercase'>
           <Link href='/'>
-          <li className='py-4 text-sm'>Home</li>
+            <li onClick={() => setNav(false)}className='py-4 text-sm'>Home</li>
           </Link>
-          <Link href='/'>
-          <li className='py-4 text-sm'>About</li>
+          <Link href='/#about'>
+            <li onClick={() => setNav(false)}className='py-4 text-sm'>About</li>
           </Link>
-          <Link href='/'>
-          <li className='py-4 text-sm'>Skills</li>
+          <Link href='/#skills'>
+            <li onClick={() => setNav(false)}className='py-4 text-sm'>Skills</li>
           </Link>
-          <Link href='/'>
-          <li className='py-4 text-sm'>Projects</li>
+          <Link href='/#projects'>
+            <li onClick={() => setNav(false)}className='py-4 text-sm'>Projects</li>
           </Link>
-          <Link href='/'>
-          <li className='py-4 text-sm'>Contact</li>
+          <Link href='/#contact'>
+            <li onClick={() => setNav(false)}className='py-4 text-sm'>Contact</li>
           </Link>
         </ul>
         <div className='pt-40'>
@@ -114,9 +158,7 @@ const Navbar = () => {
         </div>
       </div>
       </div>
-
     </div>
-
     </div>
   )
 }
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -8,6 +8,10 @@ import Tailwind from '../public/assets/skills/tailwind.png';
 import Github from '../public/assets/skills/github1.png';
 import Firebase from '../public/assets/skills/firebase.png';
 import NextJS from '../public/assets/skills/nextjs.png'
+import NodeJS from '../public/assets/skills/node.png'
+import MongoDB from '../public/assets/skills/mongo.png'
+import Docker from '../public/assets/skills/docker2.png'
+import Cobol from '../public/assets/skills/cobol.png'
 
 const Skills = () => {
   return (
@@ -15,7 +19,7 @@ const Skills = () => {
         <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
             <p className='uppercase text-xl tracking-widest text-[#5651e5]'>Skills</p>
             <h2 className='py-4'>What I Can Do</h2>
-            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+            <div className='grid md:grid-cols-3 lg:grid-cols-4 gap-8'>
 
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
@@ -97,8 +101,46 @@ const Skills = () => {
                         </div>
                     </div>
                 </div>
-
-
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={NodeJS} width='32px' heigth='32px' alt='/' />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Node.js</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={MongoDB} width='32px' heigth='32px' alt='/' />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>MongoDB</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={Docker} width='32px' heigth='32px' alt='/' />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Docker</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                        <div className='m-auto'>
+                            <Image src={Cobol} width='32px' heigth='32px' alt='/' />
+                        </div>
+                        <div className='flex flex-col items-center justify-center'>
+                            <h3>Cobol</h3>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
